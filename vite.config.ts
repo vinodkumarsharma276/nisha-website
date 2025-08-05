192331a15facad4f6ef0558a0ff101921deeb6ad
Vinod Kumar Sharma <vinodsharma@microsoft.com>
2025-08-06 02:44:03 +0530

    Setup GitHub Pages deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nisha-website/',
   server: {
     port: 8173
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
