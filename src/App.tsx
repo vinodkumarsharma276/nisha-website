868ec420b97532683ed7e1a49877c9b3da47e822
Vinod Kumar Sharma <vinodsharma@microsoft.com>
2025-08-09 00:45:11 +0530

    Changed Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Header from './components/Header';
-import Home from './components/Home';
-import Services from './components/Services';
-import Blogs from './components/Blogs';
-import Projects from './components/Projects';
-import Experience from './components/Experience';
-import Contact from './components/Contact';
+import Header from './components/Header.tsx';
+import Home from './components/Home.tsx';
+import Services from './components/Services.tsx';
+import Blogs from './components/Blogs.tsx';
+import Projects from './components/Projects.tsx';
+import Experience from './components/Experience.tsx';
+import Contact from './components/Contact.tsx';
 
 function App() {
   return (
