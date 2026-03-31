fbb5cd7d54a53475fa86c33abb26096c9ca0f857
vinodkumarsharma276 <vinodkumarsharma276@gmail.com>
2026-04-01 02:20:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 declare const process: { env?: Record<string,string | undefined> } | undefined;
 
 const target = process?.env?.BUILD_TARGET;
-const base = target === 'azure' ? '/' : '/nisha-website/';
+const base = target === 'azure' ? '/' : '/';
 
 export default defineConfig({
   plugins: [react()],
